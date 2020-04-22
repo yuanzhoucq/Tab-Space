df05646c673a83652096cb73fca2f86ca99ee6b5
Yuan ZHOU <yuanzhou.cq@gmail.com>
2020-04-22 13:25:02 +0800

    add Danish support thanks to aamoussa97@gmail.com

--- admin/src/constants.js
+++ admin/src/constants.js
@@ -8,6 +8,7 @@ module.exports = {
     preferredLanguageKey: "preferred-language",
     languages: [
         {code: "zh-cn", name: "Chinese"},
+        {code: "da", name: "Danish"},
         {code: "en-us", name: "English"},
         {code: "es", name: "Spanish"},
         {code: "uk", name: "Ukrainian"},
